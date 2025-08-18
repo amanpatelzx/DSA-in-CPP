11f0f825513a004fab222794fd3797e575c141b5
Aman Patel <a24032002p2@gmail.com>
2025-08-18 20:10:28 +0530

    ....

--- Oops 1/14.cpp
+++
@@ -1,7 +0,0 @@
-#include<iostream>
-using namespace std;
-int main(){
-    int x=3;
-    cout<<&x;//this & give location of addess where this
-    }        //is asigned in memory
-             //it will keep changing on every times run
